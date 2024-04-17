42c70b0ad57ceffc86ee240640085d7ff73d3461
ryan-arbar <144367453+ryan-arbar@users.noreply.github.com>
2024-04-16 20:50:22 -0700

    Delete Assets/Scripts/TODO.cs

--- Assets/Scripts/TODO.cs
+++
@@ -1,4 +0,0 @@
-//Update the Tier1BaseRoom so that the DoorInteraction script can
-//properly identify the Player object automatically
-
-//Fix the issue of the game determining which room is the current room
